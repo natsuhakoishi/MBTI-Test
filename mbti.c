23e160db9059d71abf9550d28bef848ff5aaca8f
Yap Yean Wah <yyean-wa@u91z03s01.42kl.edu.my>
2024-12-06 22:50:46 +0800

    UX

--- mbti.c
+++ mbti.c
@@ -36,6 +36,7 @@ int	main(int argc, char **argv)
 	char	*line;
 	int		input;
 	int		ques_count = 0;
+	char	c;
 
 	(void)argv;
 	if (argc != 1)
@@ -50,22 +51,46 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	printf("\nWelcome to MBTI Test Program by NatsuhaKoishi\n");
-	printf("Answer all (total 150) Question with a score between 1 ~ 10.\n\n");
+	printf("Answer all (total 150) Question with a score between 1 ~ 9.\n");
+	printf("Scoring tips: 1 for Strongly Disagree, 5 for Neutral, 9 for Strongly Agree.\n\n");
+	printf("Do you ready to answer the question? (y/n): ");
+	scanf("%c", &c);
+	while (c != 'y' && c != 'Y' && c != 'n' && c != 'N')
+	{
+		printf("\nInvalid input.\nPlease enter a valid character (y/n): ");
+		if (scanf(" %c", &c) != 1)
+		{
+			while (getchar() != '\n')
+			{
+				continue;
+			}
+		}
+		else
+		{
+			getchar();
+		}
+	}
+	if (c == 'n' || c == 'N')
+	{
+		printf("\nSee you later !\n\n");
+		return (1);
+	}
+
 	while ((line = get_next_line(fileno(question))) != NULL)
 	{
 		input = 0;
 		printf("\nQuestion %d: %s\n", ++ques_count, line);
 		free(line);
-		printf("Your answer (1~10) > ");
+		printf("Your answer (1~9) > ");
 		scanf("%d", &input);
-		while (input < 1 || input > 10)
+		while (input < 1 || input > 9)
 		{
-			printf("\nInvalid input.\nPlease enter a valid number (1~10) > ");
+			printf("\nInvalid input.\nPlease enter a valid number (1~9) > ");
 			if (scanf("%d", &input) != 1)
 			{
 				while (getchar() != '\n')
 				{
-					printf("\nInvalid input.\nPlease enter a valid number (1~10) > ");
+					printf("\nInvalid input.\nPlease enter a valid number (1~9) > ");
 					continue;
 				}
 			}
@@ -77,19 +102,19 @@ int	main(int argc, char **argv)
 				ques_count == 142) //E vs I
 		{
 			score[0] += input;
-			max[0] += 10;
+			max[0] += 9;
 		}
 		else if ((ques_count >= 21 && ques_count <= 40) || (ques_count >= 91 && ques_count <= 100) || \
 					ques_count == 144 || ques_count == 145 || ques_count == 148) //S vs N
 		{
 			score[1] += input;
-			max[1] += 10;
+			max[1] += 9;
 		}
 		else if ((ques_count >= 41 && ques_count <= 60) || (ques_count >= 101 && ques_count <= 110) || \
 					ques_count == 124 || ques_count == 149) //T vs F
 		{
 			score[2] += input;
-			max[2] += 10;
+			max[2] += 9;
 		}
 		else if ((ques_count >= 61 && ques_count <= 80) || (ques_count >= 111 && ques_count <= 120) || \
 				ques_count == 123 || ques_count == 125 || ques_count == 127 || ques_count == 128 || \
@@ -97,22 +122,22 @@ int	main(int argc, char **argv)
 				ques_count == 141 || ques_count == 146 || ques_count == 150) //J vs P
 		{
 			score[3] += input;
-			max[3] += 10;
+			max[3] += 9;
 		}
 		else if (ques_count == 130 || ques_count == 143)
 		{
-			score[0] += 10 - input;
-			max[0] += 10;
+			score[0] += 9 - input;
+			max[0] += 9;
 		}
 		else if (ques_count == 147)
 		{
-			score[2] += 10 - input;
-			max[2] += 10;
+			score[2] += 9 - input;
+			max[2] += 9;
 		}
 		else if (ques_count == 122 || ques_count == 126)
 		{
-			score[3] += 10 - input;
-			max[3] += 10;
+			score[3] += 9 - input;
+			max[3] += 9;
 		}
 		else if (ques_count > 150)
 		{
